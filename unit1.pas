10ccb8d06c8008cb37a1e6aa1fb5736fa5a5dd1a
AhmadNurulAziz <ahmadnurulaziz@gmail.com>
2015-06-12 05:15:12 +0700

    tugas 2 pd

---
+++ unit1.pas
@@ -0,0 +1,48 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AhmadNurulAziz: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.caption:= AhmadNurulAziz.text;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Label1.caption:='';
+  AhmadNurulaziz.clear;
+end;
+
+end.
+
